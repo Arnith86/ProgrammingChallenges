49e1b2d675c79b39579a917611d04b0e38433f4c
a21jeaha <hisui86@gmail.com>
2024-09-14 17:05:33 +0200

    fillShop was refactored and the method putEquipmentInList added

--- RPGSimulator20XX/RPGSimulator20XX.cs
+++ RPGSimulator20XX/RPGSimulator20XX.cs
@@ -41,11 +41,12 @@ public class RPGSimulator20XX
 			default:
 				break;
 		}
+		else return false;
 	}
 
+	// Fills shop with equipments found in a file.
 	private void fillShop()
 	{
-		int nrOfLines = (File.ReadLines("ItemShop.txt").Count());
 		StreamReader sr = new StreamReader("ItemShop.txt");
 
 		string line;
@@ -56,31 +57,70 @@ public class RPGSimulator20XX
 			if (line.Contains("Weapons:")) 
 			{
 				equipmentType = "Weapon";
-
-				while (!string.IsNullOrEmpty(line = sr.ReadLine()))
-				{
-					putEquipmentInList(equipmentType, line);
-				}
+				while (!string.IsNullOrEmpty(line = sr.ReadLine()))	putEquipmentInList(equipmentType, line);
 			}
 
 			if (line.Contains("Armor:"))
 			{
 				equipmentType = "Armor";
-				while (!string.IsNullOrEmpty(line = sr.ReadLine()))
-				{
-					putEquipmentInList(equipmentType, line);
-				}
+				while (!string.IsNullOrEmpty(line = sr.ReadLine())) putEquipmentInList(equipmentType, line);
 			}
 
 			if (line.Contains("Rings:"))
 			{
 				equipmentType = "Ring";
-				while (!string.IsNullOrEmpty(line = sr.ReadLine()))
-				{
-					putEquipmentInList(equipmentType, line);
-				}
+				while (!string.IsNullOrEmpty(line = sr.ReadLine())) putEquipmentInList(equipmentType, line);
 			}
 		}
+	}
+
+	// Places equipments in its appropriate list 
+	private void putEquipmentInList(string equipmentType, string readLine)
+	{
+		// Replace multiple spaces with a single space
+		string normalizedReadLine = Regex.Replace(readLine, @"\s+", "  ");
+		string[] lineToArray = normalizedReadLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+		string type = lineToArray[0];
+		int cost = int.Parse(lineToArray[1]);
+		int damage = int.Parse(lineToArray[2]);
+		int armor = int.Parse(lineToArray[3]);
+
+		switch (equipmentType)
+		{
+			case "Weapon":
+				weapons.Add(new Equipment(type, cost, damage, armor));
+				break;
+			case "Armor":
+				armors.Add(new Equipment(type, cost, damage, armor));
+				break;
+			case "Ring":
+				rings.Add(new Equipment(type, cost, damage, armor));
+				break;
+			default:
+				break;
+		}
+	}
+
+	// Gets the stats for the boss
+	private int[] readBossStats()
+	{
+		int[] bossStats= new int[3];
+		StreamReader sr = new StreamReader("RPGSimulator20XXInput.txt");
+		string line;
+
+		// This regex will match one or more digits (i.e., an integers)
+		Regex regex = new Regex(@"\d+");
+
+		for (int i = 0; i < 3; i++)
+		{
+			line = sr.ReadLine();
+			// finds integers in the line
+			Match integers = regex.Match(line);
+			if (integers.Success) bossStats[i] = int.Parse(integers.Value); 
+		}
+
+		return bossStats;
 	}
 }
 
