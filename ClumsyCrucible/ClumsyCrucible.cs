8e08f267f3bd79022eddf937037546747ab5c1c8
a21jeaha <hisui86@gmail.com>
2024-09-19 14:18:18 +0200

    initial commit for challenge Clumsy Crucible

---
+++ ClumsyCrucible/ClumsyCrucible.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingChallenges.ClumsyCrucible;
+
+public class ClumsyCrucible
+{
+	int NR_OF_ROWS = 0; 
+
+	public ClumsyCrucible()
+	{
+		readInput();
+	}
+
+	private void readInput()
+	{
+		StreamReader sr = new StreamReader("ExampleInput.txt");
+		NR_OF_ROWS = sr.ReadLine().Length;
+	}
+}
+
