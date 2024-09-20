717d6ae3f77b6219ffb27b9f109373a2476d8b81
a21jeaha <hisui86@gmail.com>
2024-09-20 10:38:15 +0200

    the reading of the input and conversion to a 2d array implemented

--- ClumsyCrucible/ClumsyCrucible.cs
+++ ClumsyCrucible/ClumsyCrucible.cs
@@ -8,17 +8,40 @@ namespace ProgrammingChallenges.ClumsyCrucible;
 
 public class ClumsyCrucible
 {
-	int NR_OF_ROWS = 0; 
+	int NR_OF_ROWS;
+	int NR_OF_COLUMNS;
+
+	int[,] heatMap;  
 
 	public ClumsyCrucible()
 	{
 		readInput();
 	}
 
-	private void readInput()
+	// Gets the proportion of the input matrix
+	private void getInputSize()
 	{
 		StreamReader sr = new StreamReader("ExampleInput.txt");
-		NR_OF_ROWS = sr.ReadLine().Length;
+		NR_OF_ROWS = (File.ReadLines("ExampleInput.txt").Count());
+		NR_OF_COLUMNS = sr.ReadLine().Count();
+	}
+
+	// Creates the map that will be used to map the possible routes.
+	private void readInput()
+	{
+		getInputSize();
+		StreamReader sr = new StreamReader("ExampleInput.txt");
+		heatMap = new int[NR_OF_ROWS, NR_OF_COLUMNS];
+
+		for (int i = 0; i < NR_OF_ROWS; i++)
+		{
+			string line = sr.ReadLine();
+			int index = 0; 
+			foreach (char charInString in line)
+			{
+				heatMap[i, index++] = (int)char.GetNumericValue(charInString);
+			}
+		}
 	}
 }
 
