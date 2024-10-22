d0ac994af5d6fb1925fba6b7cbf0a87ec67f8e83
a21jeaha <hisui86@gmail.com>
2024-10-22 11:14:15 +0200

    Updated code to follow instructions
    misunderstood the instructions in the earlier version.

--- ArithmeticLogicUnit/ArithmeticLogicUnit.cs
+++ ArithmeticLogicUnit/ArithmeticLogicUnit.cs
@@ -12,13 +12,9 @@ public class ArithmeticLogicUnit
 	int NR_OF_ROWS = 0;
 	ALU alu;
 
-	Dictionary<int, ModelNr> MONAD;
-
 	public ArithmeticLogicUnit() 
 	{
-		MONAD = new Dictionary<int, ModelNr>();
-		alu = new ALU(MONAD, readInput(), NR_OF_ROWS);
-		
+		alu = new ALU(/*MONAD,*/ readInput(), NR_OF_ROWS);
 	}
 
 	private string[] readInput()
@@ -33,63 +29,100 @@ public class ArithmeticLogicUnit
 
 public class ALU
 {
-	Dictionary<int, ModelNr> MONAD;
-	string[] allInput; 
+	string[] allInput;
 	int NR_OF_ROWS;
 
-	public ALU(Dictionary<int, ModelNr> monad, string[] allInput, int nrOfRows)
+	public ALU(string[] allInput, int nrOfRows)
 	{
-		MONAD = monad;
+		int[] modelNr = new int[14];
+
 		this.allInput = allInput;
 		NR_OF_ROWS = nrOfRows;
 
-		// Here ttemporeraly for test 
-		int modelNr = 0; 
+		Dictionary<string, int> variables = new Dictionary<string, int>
+		{
+			{"w", 0},
+			{"x", 0},
+			{"y", 0},
+			{"z", 0}
+		};
+
 
 		int index = 0;
-
-		MONAD.Add(index, new ModelNr(/*command*/));
-
-		for (int i = 0; i < NR_OF_ROWS; i++)
+		for (long currentModelNr = 99999999999999; currentModelNr > 0; currentModelNr--)
 		{
-			string[] enteredCommand = allInput[i].Split(' ');
+			Console.WriteLine(currentModelNr);
 
-			string command = enteredCommand[0];
-			string variableNameA = enteredCommand[1];
-			int a;
-			int b;
+			// Convert the long to a string, then split it into individual digits, and convert back to int array
+			modelNr = currentModelNr.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+			index = 0;
+			bool containsZero = false;
 
-			if (command.Equals("inp"))
+			int currentNr = modelNr[index];
+
+			for (int k = 0; k < NR_OF_ROWS; k++)
 			{
-				// What happens if provided variable already exists?????
-				//MONAD.Add(index, new ModelNr(/*command*/));
-				if (MONAD[index].Variables.ContainsKey(variableNameA)) MONAD[index].Variables[variableNameA] = whichVariableGet(index, enteredCommand[1]);
-				else setValue(index, enteredCommand[1], MONAD[index].Variables[variableNameA]);
-				//int a = whichVariableGet(index, enteredCommand[1]);
+				string[] enteredCommand = allInput[k].Split(' ');
+
+				string command = enteredCommand[0];
+				string variableNameA = enteredCommand[1];
+				int a;
+				int b;
+
+				// Handles the input command  
+				if (command.Equals("inp"))
+				{
+					if (modelNr[index] == 0)
+					{
+						containsZero = true;
+						break;
+					}
+					else
+					{
+						variables[variableNameA] = modelNr[index];
+						index++;
+						continue;
+					}
+				}
+				else
+				{
+					// Keep track of variable name and value
+					a = variables[enteredCommand[1]];
+
+					// Uses the number value provided or collects saved value of variable
+					if (enteredCommand[2].Any(Char.IsNumber)) b = int.Parse(enteredCommand[2]);
+					else b = variables[enteredCommand[2]];
+
+					// Perform logic operation
+					a = logic(command, a, b);
+
+					// Save value in variable
+					variables[variableNameA] = a;
+				}
+			}
+
+			// Valid modelNr check 
+			if (variables["z"] == 0 && !containsZero)
+			{
+				break;
+			}
+			else if (containsZero)
+			{
 				continue;
 			}
-			else
+		}
+
+		// WILL "z" naturally contain 0?
+		if (variables["z"] == 0)
+		{
+			for (int i = 0; i < 14; i++)
 			{
-				// Keep track of variable name and value
-				a = whichVariableGet(index, enteredCommand[1]);
-
-				if (enteredCommand[2].Any(Char.IsNumber)) b = int.Parse(enteredCommand[2]);
-				else b = whichVariableGet(index, enteredCommand[2]);
-
-				a = logic(index, command, a, b);
-
-				MONAD[index].Variables[variableNameA] = a;
-			}
-
-			MONAD[index].buildNr();
-			if (MONAD[index].Variables.Count() == 14)
-			{
-				Console.WriteLine("its 14");
+				Console.WriteLine($"{modelNr[0]}");
 			}
 		}
 	}
 
-	public int logic(int index, string command, int a, int b)
+	public int logic(string command, int a, int b)
 	{
 		// handle devison by 0, and modulo by 0 
 		switch (command)
@@ -99,9 +132,9 @@ public class ALU
 			case "mul":
 				a *= b; break;
 			case "div":
-				a /= b;  break;
+				a /= b; break;
 			case "mod":
-				a %= b;  break;
+				a %= b; break;
 			case "eql":
 				a = a == b ? 1 : 0; break;
 			default:
@@ -109,82 +142,4 @@ public class ALU
 		}
 		return a;
 	}
-
-	private int whichVariableGet(int index, string variable)
-	{
-		return MONAD[index].Variables[variable];
-	}
-
-	//private void initiateVariable(int index, string variable, int value)
-	//{
-	//	MONAD[index].Variables.Add(variable, value);
-	//}
-
-	private void setValue(int index, string variable, int value)
-	{
-		MONAD[index].Variables[variable] = value;
-	}
 }
-
-public class ModelNr
-{
-	public string Nr { get; set; }
-	
-	public Dictionary<string, int> Variables { get; set; }
-	
-	public ModelNr()
-	{
-		Variables = new Dictionary<string, int> 
-		{
-			{"w", 0},
-			{"x", 0},
-			{"y", 0},
-			{"z", 0}
-		};
-	}
-
-	public void buildNr()
-	{
-		Nr = $"{Variables["w"]}{Variables["x"]}{Variables["y"]}{Variables["z"]}";
-	}
-}
-
-//switch (variable)
-//{
-//	case "w":
-//		MONAD[index].Variables.Add(variable, value); break; // MONAD[index].W = value; break;
-//	case "x":
-//		MONAD[index].X = value; break;
-//	case "y":
-//		MONAD[index].Y = value; break;
-//	case "z":
-//		MONAD[index].Z = value break;
-//	default:
-//		break;
-//}
-
-
-//public int Add(int a, int b)
-//{
-//	return a += b;
-//}
-
-//public int Mul(int a, int b)
-//{
-//	return a = a * b;
-//}
-
-//public int Div(int a, int b)
-//{
-//	return a = a / b;
-//}
-
-//public int Mod(int a, int b)
-//{
-//	return a = a % b;
-//}
-
-//public int eql(int a, int b)
-//{
-//	return a == b ? 1 : 0;
-//}
